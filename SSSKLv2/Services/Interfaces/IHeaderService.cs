c44878a749fc90cd87229dd080a5af466c810439
Ricardo Tillemans <me@ricardotill.nl>
2024-04-08 13:21:57 +0200

    nav-menu default color grey, when saldo negative red

--- SSSKLv2/Services/Interfaces/IHeaderService.cs
+++ SSSKLv2/Services/Interfaces/IHeaderService.cs
@@ -3,6 +3,7 @@ namespace SSSKLv2.Services.Interfaces;
 public interface IHeaderService
 {
     public string Saldo { get; set; }
+    public bool SaldoNegative => Saldo.Contains("-");
     public string Name { get; set; }
     public event EventHandler? HeaderChanged;
     public void SetName(string name);
